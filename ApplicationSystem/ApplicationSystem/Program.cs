d51c29b0d7726cfcea7e5443fce8abebf1c702fc
Kareem <kareemeldessoukycivil@gmail.com>
2023-09-24 11:16:37 +0200

    Starting-Project

---
+++ ApplicationSystem/ApplicationSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
